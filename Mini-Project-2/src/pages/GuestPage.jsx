835fc98c8fdfbf2242271e9c6e751f9e036b82d9
emma-landry7 <emmablandry730@gmail.com>
2024-01-25 22:39:38 -0600

    added search bar to landing page

--- Mini-Project-2/src/pages/GuestPage.jsx
+++ Mini-Project-2/src/pages/GuestPage.jsx
@@ -8,9 +8,15 @@ export default function GuestPage() {
             <section>
                 <article className="margin">
                     Welcome to Cosmic Strip, the website where you can find all your favorite webcomics in one place!
-                    Log in to search, comment, and post!
+                    Log in to comment and post!
                 </article>
                 <Link to="/login"><button>LOG IN</button></Link>
+            </section>
+            <section className="margin">
+                <label>
+                    <input type="text" value="Search" name="search"
+                        onChange={(e) => e.filter(a => a.target.value)} />
+                </label>
             </section>
             <section>
                 <h3 className="featured margin">Featured Posts</h3>
